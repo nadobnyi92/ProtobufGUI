654bbfb2ca74290be83c35101d16938cb2969c66
nadobnyi <nadobny92@gmail.com>
2019-09-17 08:05:06 +0300

    add float protoitem

---
+++ src/floatprotoitem.cpp
@@ -0,0 +1,41 @@
+#include "floatprotoitem.h"
+
+#include <QDoubleSpinBox>
+
+FloatProtoItem::ItemDelegate::ItemDelegate(QObject *parent) : QItemDelegate(parent) {}
+
+QWidget *FloatProtoItem::ItemDelegate::createEditor(QWidget *parent, const QStyleOptionViewItem &, const QModelIndex &) const
+{
+    return new QDoubleSpinBox(parent);
+}
+
+void FloatProtoItem::ItemDelegate::setEditorData(QWidget *editor, const QModelIndex &index) const
+{
+    QDoubleSpinBox * e = static_cast<QDoubleSpinBox*>(editor);
+    e->setValue(index.model()->data(index).toDouble());
+}
+
+void FloatProtoItem::ItemDelegate::setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const
+{
+    QDoubleSpinBox * e = static_cast<QDoubleSpinBox*>(editor);
+    model->setData(index, e->value());
+}
+
+FloatProtoItem::FloatProtoItem(const proto::FieldDescriptor * field, ProtoTreeItem *parentItem)
+    : ProtoTreeItem(field, parentItem), mDelegate(new ItemDelegate) {}
+
+FloatProtoItem::~FloatProtoItem()
+{
+    delete mDelegate;
+}
+
+
+QItemDelegate *FloatProtoItem::getDelegate() const
+{
+    return mDelegate;
+}
+
+QBrush FloatProtoItem::color() const
+{
+    return QBrush(QColor(0, 0, 255, 90));
+}
---
+++ src/floatprotoitem.h
@@ -0,0 +1,33 @@
+#ifndef FLOATPROTOITEM_H
+#define FLOATPROTOITEM_H
+
+#include <QItemDelegate>
+
+#include "prototreeitem.h"
+
+class FloatProtoItem : public ProtoTreeItem
+{
+    class ItemDelegate : public QItemDelegate
+    {
+        public:
+            ItemDelegate(QObject * parent = nullptr);
+
+            QWidget *createEditor(QWidget *parent, const QStyleOptionViewItem &option, const QModelIndex &index) const override;
+            void setEditorData(QWidget *editor, const QModelIndex &index) const override;
+            void setModelData(QWidget *editor, QAbstractItemModel *model, const QModelIndex &index) const override;
+    };
+
+public:
+    FloatProtoItem(const proto::FieldDescriptor * field, ProtoTreeItem *parentItem = nullptr);
+    ~FloatProtoItem() override;
+
+    // ProtoTreeItem interface
+public:
+    QItemDelegate *getDelegate() const override;
+    QBrush color() const override;
+
+protected:
+    ItemDelegate *mDelegate;
+};
+
+#endif // FLOATPROTOITEM_H
