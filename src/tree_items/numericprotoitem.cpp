f3a8f2626c6374334cb5739120a2cb0636031ea0
nadobnyi <nadobny92@gmail.com>
2020-03-08 18:14:18 +0300

    create field wrapper

--- src/tree_items/numericprotoitem.cpp
+++ src/tree_items/numericprotoitem.cpp
@@ -1,38 +1,15 @@
 #include "numericprotoitem.h"
 
+#include <any>
+
 #include <QSpinBox>
 
-#include <variant>
+#include "protofieldwrapper.h"
 
-template <typename T>
-struct IntegerType
-{
-    T(proto::Reflection::*get_int)(const proto::Message&, const proto::FieldDescriptor*) const;
-    T(proto::Reflection::*get_repeated_int)(const proto::Message&, const proto::FieldDescriptor*, int) const;
-    void(proto::Reflection::*set_int)(proto::Message*, const proto::FieldDescriptor*, T) const;
-    void(proto::Reflection::*set_repeated_int)(proto::Message*, const proto::FieldDescriptor*, T) const;
-    int minValue;
-    int maxValue;
-};
-
-#define createIntegerType(N1, N2) \
-    IntegerType<proto::N1> {&proto::Reflection::Get##N2, \
-                            &proto::Reflection::GetRepeated##N2, \
-                            &proto::Reflection::Set##N2, \
-                            &proto::Reflection::Add##N2}
-
-using IType = std::variant<
-    IntegerType<proto::int32>,
-    IntegerType<proto::int64>,
-    IntegerType<proto::uint32>,
-    IntegerType<proto::uint64> >;
-
-static std::map<proto::FieldDescriptor::CppType, IType> mTypes {
-    { proto::FieldDescriptor::CPPTYPE_INT32, createIntegerType(int32, Int32) },
-    { proto::FieldDescriptor::CPPTYPE_INT64, createIntegerType(int64, Int64) },
-    { proto::FieldDescriptor::CPPTYPE_UINT32, createIntegerType(uint32, UInt32) },
-    { proto::FieldDescriptor::CPPTYPE_UINT64, createIntegerType(uint64, UInt64) }
-};
+createWrapper(proto::int32, Int32, INT32)
+createWrapper(proto::int64, Int64, INT64)
+createWrapper(proto::uint32, UInt32, UINT32)
+createWrapper(proto::uint64, UInt64, UINT64)
 
 NumericProtoItem::ItemDelegate::ItemDelegate(QObject *parent)
     : QItemDelegate(parent) {}
@@ -73,40 +50,34 @@ QItemDelegate *NumericProtoItem::getDelegate() const
     return mDelegate;
 }
 
-
 void NumericProtoItem::fillFieldValue(google::protobuf::Message *message)
 {
-    auto func = [&](auto& a){
-        (message->GetReflection()->*a.set_int)(message, field(), value().toInt());
-    };
-    std::visit(func, mTypes[field()->cpp_type()]);
+    auto fw = FieldWrapperRegister::get().wrapper(field()->cpp_type());
+    if(fw != nullptr) {
+        fw->setFieldData(message, field(), value());
+    }
 }
-
 
 void NumericProtoItem::addFieldValue(google::protobuf::Message * message, const google::protobuf::FieldDescriptor * desk)
 {
-    auto func = [&](auto& a){
-        (message->GetReflection()->*a.set_repeated_int)(message, desk, value().toInt());
-    };
-    std::visit(func, mTypes[desk->cpp_type()]);
+    auto fw = FieldWrapperRegister::get().wrapper(desk->cpp_type());
+    if(fw != nullptr) {
+        fw->setRepeatedFieldData(message, desk, value());
+    }
 }
-
 
 void NumericProtoItem::initFieldValue(const google::protobuf::Message * message)
 {
-    auto func = [&](auto& a){
-        auto res = (message->GetReflection()->*a.get_int)(*message, field());
-        return QVariant::fromValue(res);
-    };
-    setValue(std::visit(func, mTypes[field()->cpp_type()]));
+    auto fw = FieldWrapperRegister::get().wrapper(field()->cpp_type());
+    if(fw != nullptr) {
+        setValue(fw->getFieldData(*message, field()));
+    }
 }
-
 
 void NumericProtoItem::initRepeatedFieldValue(const google::protobuf::Message * message, int idx)
 {
-    auto func = [&](auto& a) {
-        auto res = (message->GetReflection()->*a.get_repeated_int)(*message, field(), idx);
-        return QVariant::fromValue(res);
-    };
-    setValue(std::visit(func, mTypes[field()->cpp_type()]));
+    auto fw = FieldWrapperRegister::get().wrapper(field()->cpp_type());
+    if(fw != nullptr) {
+        setValue(fw->getRepeatedFieldData(*message, field(), idx));
+    }
 }
---
+++ src/tree_items/protofieldwrapper.h
@@ -0,0 +1,108 @@
+#ifndef PROTOFIELDWRAPPER_H
+#define PROTOFIELDWRAPPER_H
+
+#include <google/protobuf/dynamic_message.h>
+
+#include <QVariant>
+
+namespace proto = google::protobuf;
+using protofd = google::protobuf::FieldDescriptor;
+
+template <typename T>
+struct FieldWrapperAccess
+{
+    T(proto::Reflection::*get)(const proto::Message&, const proto::FieldDescriptor*) const;
+    T(proto::Reflection::*get_repeated)(const proto::Message&, const proto::FieldDescriptor*, int) const;
+    void(proto::Reflection::*set)(proto::Message*, const proto::FieldDescriptor*, T) const;
+    void(proto::Reflection::*set_repeated)(proto::Message*, const proto::FieldDescriptor*, T) const;
+    T minValue;
+    T maxValue;
+    QVariant getFieldData(const proto::Message& message, const proto::FieldDescriptor* field){
+        return QVariant::fromValue((message.GetReflection()->*get)(message, field));
+    }
+    QVariant getRepeatedFieldData(const proto::Message& message, const proto::FieldDescriptor* field, int idx) {
+        return QVariant::fromValue((message.GetReflection()->*get_repeated)(message, field, idx));
+    }
+    void setFieldData(proto::Message* message, const proto::FieldDescriptor* field, const QVariant& data) {
+        (message->GetReflection()->*set)(message, field, qvariant_cast<T>(data));
+    }
+    void setRepeatedFieldData(proto::Message* message, const proto::FieldDescriptor* field, const QVariant& data) {
+        (message->GetReflection()->*set_repeated)(message, field, qvariant_cast<T>(data));
+    }
+};
+
+class FieldWrapper
+{
+public:
+    virtual ~FieldWrapper() {}
+    virtual QVariant getFieldData(const proto::Message& message, const proto::FieldDescriptor* field) = 0;
+    virtual QVariant getRepeatedFieldData(const proto::Message& message, const proto::FieldDescriptor* field, int idx) = 0;
+    virtual void setFieldData(proto::Message* message, const proto::FieldDescriptor* field, const QVariant& data) = 0;
+    virtual void setRepeatedFieldData(proto::Message* message, const proto::FieldDescriptor* field, const QVariant& data) = 0;
+};
+
+class FieldWrapperRegister
+{
+    FieldWrapperRegister() {}
+public:
+    FieldWrapperRegister(const FieldWrapperRegister&) = delete;
+    FieldWrapperRegister(FieldWrapperRegister&&) = delete;
+    FieldWrapperRegister& operator=(const FieldWrapperRegister&) = delete;
+    FieldWrapperRegister& operator=(FieldWrapperRegister&&) = delete;
+
+    static FieldWrapperRegister& get() {
+        static FieldWrapperRegister rg;
+        return rg;
+    }
+
+    bool regWrapper(protofd::CppType type, FieldWrapper* wrapper) {
+        bool isReg = false;
+        if(auto it = mWrappers.find(type); it != mWrappers.end()) {
+            mWrappers[type] = wrapper;
+            isReg = true;
+        }
+        return isReg;
+    }
+    FieldWrapper* wrapper(protofd::CppType type) {
+        FieldWrapper * wrapper = nullptr;
+        if(auto it = mWrappers.find(type); it != mWrappers.end()) {
+            wrapper = it->second;
+        }
+        return wrapper;
+    }
+
+private:
+    std::map<protofd::CppType, FieldWrapper*> mWrappers;
+};
+
+#define createWrapper(N1, N2, N3) \
+    class FieldWrapper##N3 : public FieldWrapper \
+    { \
+    private: \
+        FieldWrapperAccess<N1> fw; \
+    public: \
+        FieldWrapper##N3 () { \
+            fw = FieldWrapperAccess<N1> { &proto::Reflection::Get##N2, \
+                                          &proto::Reflection::GetRepeated##N2, \
+                                          &proto::Reflection::Set##N2, \
+                                          &proto::Reflection::Add##N2, \
+                                          std::numeric_limits<N1>::min(), \
+                                          std::numeric_limits<N1>::max() }; \
+        } \
+        QVariant getFieldData(const proto::Message& message, const proto::FieldDescriptor* field) { \
+            return fw.getFieldData(message, field); \
+        } \
+        QVariant getRepeatedFieldData(const proto::Message& message, const proto::FieldDescriptor* field, int idx) {\
+            return fw.getRepeatedFieldData(message, field, idx); \
+        }\
+        void setFieldData(proto::Message* message, const proto::FieldDescriptor* field, const QVariant& data) {\
+            fw.setFieldData(message, field, data); \
+        } \
+        void setRepeatedFieldData(proto::Message* message, const proto::FieldDescriptor* field, const QVariant& data) {\
+            fw.setRepeatedFieldData(message, field, data); \
+        } \
+    }; \
+    static bool IS_REG_CPP_TYPE_##N3 = FieldWrapperRegister::get().regWrapper(protofd::CPPTYPE_##N3, new FieldWrapper##N3);
+
+
+#endif // PROTOFIELDWRAPPER_H
